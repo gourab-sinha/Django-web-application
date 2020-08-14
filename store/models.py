1a34bb4211a2214bd656dd063c27c08cef236154
gourab-sinha <gourab19964u@gmail.com>
2020-08-15 01:52:51 +0530

    Add to cart functionality added

--- store/models.py
+++ store/models.py
@@ -30,6 +30,18 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
 
+    @property
+    def get_cart_total(self):
+        order_items = self.orderitem_set.all()
+        total = sum([item.get_total for item in order_items])
+        return total
+
+    @property
+    def get_cart_items(self):
+        order_items = self.orderitem_set.all()
+        total = sum([item.quantity for item in order_items])
+        return total
+
 
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
@@ -38,7 +50,12 @@ class OrderItem(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.id
+        return str(self.id)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
--- store/views.py
+++ store/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 from store.models import *
+from django.http import JsonResponse
+import json
+
+
+def get_context(request):
+    if request.user.is_authenticated:
+        customer = request.user.id
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cart_items = order.get_cart_items
+        products = Product.objects.all()
+        context = {'products': products, 'cart_items': cart_items, 'order': order, 'items': items}
+        return context
+
+    products = Product.objects.all()
+    items = []
+    order = {'get_cart_items': 0, 'get_cart_total': 0}
+    cart_items = order['get_cart_items']
+    context = {'products': products, 'cart_items': cart_items, 'order': order, 'items': items }
+    return context
 
 
 def home(request):
@@ -8,40 +28,38 @@ def home(request):
             products = Product.objects.filter(seller=request.user.id)
             return render(request, 'store/store.html', {'products': products})
 
-    products = Product.objects.all()
-    return render(request, 'store/store.html', {'products': products})
+    context = get_context(request)
+    return render(request, 'store/store.html', context)
 
 
 def cart(request):
-    context = {}
+    context = get_context(request)
     return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-    context = {}
+    context = get_context(request)
     return render(request, 'store/checkout.html', context)
 
 
-def add_product(request):
-    pass
+def update_item(request):
+    data = json.loads(request.body)
+    product_id = data['productId']
+    action = data['action']
 
+    customer = request.user.id
+    product = Product.objects.get(id=product_id)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
 
-def update_product(request):
-    pass
+    order_item, created = OrderItem.objects.get_or_create(order=order, product=product)
 
+    if action == 'add':
+        order_item.quantity = (order_item.quantity +1)
+    elif action == 'remove':
+        order_item.quantity = (order_item.quantity - 1)
 
-def delete_product(request):
-    pass
+    order_item.save()
+    if order_item.quantity <= 0:
+        order_item.delete()
 
-
-def add_to_cart(request):
-    pass
-
-
-def place_order(request):
-    pass
-
-
-
-
-
+    return JsonResponse('Item was added', safe=False)
