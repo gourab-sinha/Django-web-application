b7d19443c95725daac0bb3e403be4a386351005b
gourab-sinha <gourab19964u@gmail.com>
2020-08-17 03:03:05 +0530

    updated settings.py

--- Dphi/settings.py
+++ Dphi/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mf&#n5eg2)#3l2l72515gt0)my(caq7gsoq5tqykb!wdb517g6'
-
+# SECRET_KEY='0263a2ab32ccf68848d7407228207a40d380ea41ee0a798d'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
